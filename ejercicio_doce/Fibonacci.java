6eb09a9090940dbde60825647d8765776507e744
Luis Miguel Aranguren Ardila <miguelo.5@hotmail.com>
2023-09-12 12:09:37 -0500

    en este pr se trabajaron 3 ejercicios el ultimo video que se vio de estos ejercicios era el 16

---
+++ ejercicio_doce/Fibonacci.java
@@ -0,0 +1,58 @@
+public class Fibonacci{
+  public static void main(String args[]){
+
+    int a = 0 , b = 1, c = 0;
+
+    System.out.println("serie con for: ");
+  
+    for(int i = 0 ; i < 10; i++){
+      if(i < 9){
+        System.out.print(a + ", ");
+        c = a + b;
+        a = b;
+        b = c;
+      }else{
+        System.out.print(a);
+      }
+    }
+
+    int i = 0;
+    System.out.println("");
+    i = 0;
+    b = 1;
+    a = 0;
+    c = 0;
+    System.out.println("serie con while: ");
+
+    while(i < 10 ){
+      if(i < 9){
+        System.out.print(a + ", ");
+        c = a + b;
+        a = b;
+        b = c;
+      }else{
+        System.out.print(a);
+      }
+      i++;
+    }
+
+    System.out.println("");
+    i = 0;
+    b = 1;
+    a = 0;
+    c = 0;
+    System.out.println("serie con do-while: ");
+
+    do{
+      if(i < 9){
+        System.out.print(a + ", ");
+        c = a + b;
+        a = b;
+        b = c;
+      }else{
+        System.out.print(a);
+      }
+      i++;
+    }while(i < 10);
+  }
+}
